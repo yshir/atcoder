f2130988abc799b5ab5492e1c94f1e578e48b39f
yshir <yshir0020@gmail.com>
2025-09-29 19:52:26 +0900

    add lib

--- .lib/lib.js
+++ .lib/lib.js
@@ -113,3 +113,38 @@ class Queue {
     return this.#stackPush.length === 0 && this.#stackPop.length === 0;
   }
 }
+
+class UnionFind {
+  constructor(n) {
+    if (n <= 0) throw new RangeError(n);
+    this.parent = Array.from({ length: n }, (_, i) => i); // parent が自分自身の場合は root
+    this.size = Array(n).fill(1);
+  }
+
+  find(x) {
+    if (x < 0 || x >= this.parent.length) throw new RangeError(x);
+    if (this.parent[x] !== x) {
+      this.parent[x] = this.find(this.parent[x]); // 経路圧縮
+    }
+    return this.parent[x];
+  }
+
+  union(x, y) {
+    let rx = this.find(x);
+    let ry = this.find(y);
+    if (rx === ry) return false;
+
+    // union by size
+    if (this.size[rx] < this.size[ry]) {
+      [rx, ry] = [ry, rx];
+    }
+
+    this.parent[ry] = rx;
+    this.size[rx] += this.size[ry];
+    return true;
+  }
+
+  same(x, y) {
+    return this.find(x) === this.find(y);
+  }
+}
