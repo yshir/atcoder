540f06469f2168755e47a4b1b16f7d4432affb9e
yshir <yshir0020@gmail.com>
2025-10-11 23:09:59 +0900

    add lib

--- .lib/lib.js
+++ .lib/lib.js
@@ -114,6 +114,78 @@ class Queue {
   }
 }
 
+class PriorityQueue {
+  #heap = [];
+
+  enqueue(priority, value) {
+    this.#heap.push({ priority, value });
+    this.#bubbleUp();
+  }
+
+  dequeue() {
+    if (this.empty()) return null;
+    const top = this.#heap[0];
+    const end = this.#heap.pop();
+    if (this.#heap.length > 0) {
+      this.#heap[0] = end;
+      this.#sinkDown();
+    }
+    return top.value;
+  }
+
+  top() {
+    return this.empty() ? null : this.#heap[0].value;
+  }
+
+  empty() {
+    return this.#heap.length === 0;
+  }
+
+  #bubbleUp() {
+    let idx = this.#heap.length - 1;
+    const element = this.#heap[idx];
+    while (idx > 0) {
+      const parentIdx = Math.floor((idx - 1) / 2);
+      const parent = this.#heap[parentIdx];
+      if (element.priority <= parent.priority) break;
+      this.#heap[parentIdx] = element;
+      this.#heap[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  #sinkDown() {
+    let idx = 0;
+    const length = this.#heap.length;
+    const element = this.#heap[0];
+    while (true) {
+      const leftIdx = 2 * idx + 1;
+      const rightIdx = 2 * idx + 2;
+      let swap = null;
+
+      if (leftIdx < length) {
+        const left = this.#heap[leftIdx];
+        if (left.priority > element.priority) swap = leftIdx;
+      }
+
+      if (rightIdx < length) {
+        const right = this.#heap[rightIdx];
+        if (
+          (swap === null && right.priority > element.priority) ||
+          (swap !== null && right.priority > this.#heap[swap].priority)
+        ) {
+          swap = rightIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.#heap[idx] = this.#heap[swap];
+      this.#heap[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
 class UnionFind {
   constructor(n) {
     if (n <= 0) throw new RangeError(n);
