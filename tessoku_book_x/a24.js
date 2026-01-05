d786a91f7b6ae0268753953e61fb5a0fd2baf4f0
yshir <yshir0020@gmail.com>
2026-01-05 12:34:51 +0900

    add answer

---
+++ tessoku_book_x/a24.js
@@ -0,0 +1,22 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+const [N] = input[0].split(' ').map(Number);
+const A = input[1].split(' ').map(Number);
+
+const lower_bound = (arr, n) => {
+  let first = 0,
+    last = arr.length - 1,
+    middle;
+  while (first <= last) {
+    middle = Math.floor((first + last) / 2);
+    if (arr[middle] < n) first = middle + 1;
+    else last = middle - 1;
+  }
+  return first;
+};
+
+const dp = [A[0]];
+for (let i = 1; i < N; i++) {
+  const idx = lower_bound(dp, A[i]);
+  dp[idx] = A[i];
+}
+console.log(dp.length);
