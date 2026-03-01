2ca0a69f532f9bf7dd61ba1c32234889f726cb7c
yshir <yshir0020@gmail.com>
2026-03-01 12:51:22 +0900

    add answer

---
+++ abc447/e.js
@@ -0,0 +1,69 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+
+const E = [];
+for (let i = 0; i < M; i++) {
+  let [u, v] = input[i + 1].split(' ').map(Number);
+  u--;
+  v--;
+  E[i] = [u, v];
+}
+
+class UnionFind {
+  constructor(n) {
+    if (n <= 0) throw new RangeError(n);
+    this.parent = Array.from({ length: n }, (_, i) => i); // parent が自分自身の場合は root
+    this.size = Array(n).fill(1);
+  }
+
+  find(x) {
+    if (x < 0 || x >= this.parent.length) throw new RangeError(x);
+    if (this.parent[x] !== x) {
+      this.parent[x] = this.find(this.parent[x]); // 経路圧縮
+    }
+    return this.parent[x];
+  }
+
+  union(x, y) {
+    let rx = this.find(x);
+    let ry = this.find(y);
+    if (rx === ry) return false;
+
+    // union by size
+    if (this.size[rx] < this.size[ry]) {
+      [rx, ry] = [ry, rx];
+    }
+
+    this.parent[ry] = rx;
+    this.size[rx] += this.size[ry];
+    return true;
+  }
+
+  same(x, y) {
+    return this.find(x) === this.find(y);
+  }
+
+  size(x) {
+    return this.size[this.find(x)];
+  }
+}
+
+const uf = new UnionFind(N);
+
+let cnt = N;
+let ans = 0n;
+for (let i = M - 1; i >= 0; i--) {
+  const [u, v] = E[i];
+
+  let ru = uf.find(u);
+  let rv = uf.find(v);
+  if (ru !== rv && cnt === 2) {
+    ans += 2n ** (BigInt(i) + 1n);
+    ans %= 998244353n;
+    continue;
+  }
+
+  uf.union(u, v);
+  if (ru !== rv) cnt--;
+}
+console.log(ans.toString());
