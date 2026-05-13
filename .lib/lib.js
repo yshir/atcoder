06bdac1f74cac893ef9697e2bd1856138493140c
yshir <yshir0020@gmail.com>
2026-05-13 23:27:18 +0900

    add lib

--- .lib/lib.js
+++ .lib/lib.js
@@ -1008,3 +1008,34 @@ class Treap {
     this.#update(x);
   }
 }
+
+/**
+ * Computes the Z-array of a string in O(n) time.
+ *
+ * @param {string} s - The input string to process.
+ * @returns {number[]} The Z-array of `s`, where `z[i]` is the length of the
+ *   longest substring starting at index `i` that is also a prefix of `s`.
+ *
+ * @example
+ * z_algorithm("aabcaabxaaaz"); // => [12, 1, 0, 0, 3, 1, 0, 0, 2, 2, 1, 0]
+ */
+function z_algorithm(s) {
+  const n = s.length;
+  const z = new Array(n).fill(0);
+  z[0] = n;
+  let l = 0,
+    r = 0;
+  for (let i = 1; i < n; i++) {
+    if (i < r) {
+      z[i] = Math.min(r - i, z[i - l]);
+    }
+    while (i + z[i] < n && s[z[i]] === s[i + z[i]]) {
+      z[i]++;
+    }
+    if (i + z[i] > r) {
+      l = i;
+      r = i + z[i];
+    }
+  }
+  return z;
+}
