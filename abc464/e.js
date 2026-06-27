5e6fbd12e65675804d5a955532ba1a95a36a5b8a
yshir <yshir0020@gmail.com>
2026-06-27 23:08:30 +0900

    add answer

--- abc464/e.js
+++ abc464/e.js
@@ -5,50 +5,30 @@ const A = [];
 for (let i = 0; i < H; i++) {
   A[i] = [];
   for (let j = 0; j < W; j++) {
-    A[i][j] = '@';
+    A[i][j] = -1;
   }
 }
 
-const B = [];
+const map = { '-1': 'A' };
 for (let i = 0; i < Q; i++) {
   let [r, c, x] = input[i + 1].split(' ');
   r = Number(r - 1);
   c = Number(c - 1);
-  B[i] = [r, c, x];
+  map[i] = x;
+  A[r][c] = i;
 }
-B.reverse();
 
-let max_i = 0;
-let max_j = 0;
-
-for (const [r, c, x] of B) {
-  if (A[r][c] !== '@') continue;
-
-  let i = r;
-  let j = c;
-  while (1) {
-    A[i][j] = x;
-    if (i > 0 && A[i - 1][j] === '@') {
-      i--;
-      continue;
-    }
-
-    if (j > 0 && A[r][j - 1] === '@') {
-      i = r;
-      j--;
-      continue;
-    }
-
-    break;
+for (let i = H - 1; i >= 0; i--) {
+  for (let j = W - 1; j > 0; j--) {
+    A[i][j - 1] = Math.max(A[i][j - 1], A[i][j]);
+  }
+}
+for (let j = W - 1; j >= 0; j--) {
+  for (let i = H - 1; i > 0; i--) {
+    A[i - 1][j] = Math.max(A[i - 1][j], A[i][j]);
   }
 }
 
 for (let i = 0; i < H; i++) {
-  for (let j = 0; j < W; j++) {
-    if (A[i][j] === '@') A[i][j] = 'A';
-  }
-}
-
-for (let i = 0; i < H; i++) {
-  console.log(A[i].join(''));
+  console.log(A[i].map((x) => map[x]).join(''));
 }
