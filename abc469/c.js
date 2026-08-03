dd74d0b401d606625ee1d236faefb57b4cabd489
yshir <yshir0020@gmail.com>
2026-08-03 19:08:37 +0900

    add answer

--- abc469/c.js
+++ abc469/c.js
@@ -2,22 +2,9 @@ const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n'
 const [N] = input[0].split(' ').map(Number);
 const S = input[1];
 
-const A = [];
-for (let i = 0; i < N; i++) {
-  A[i] = A[i - 1] || 0;
-  A[i] += S[i] === 'o' ? 1 : 0;
+let i = 0;
+for (let k = 0; k < N; k++) {
+  while (S[i] === 'o' && i < N - 1) i++;
+  console.log(i + 1);
+  if (i < N - 1) i++;
 }
-
-const ans = [];
-for (let i = 0; i < N; i++) {
-  let l = i;
-  let k = A[l];
-  let r = Math.min(l + k, N - 1);
-  while (l !== r) {
-    k = A[r] - A[l];
-    l = r;
-    r = Math.min(l + k, N - 1);
-  }
-  ans.push(r + 1);
-}
-console.log(ans.join('\n'));
