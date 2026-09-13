98c16de7ef92c123c127f1c10413c796b376abab
yshir <yshir0020@gmail.com>
2026-09-13 15:34:12 +0900

    add answer

--- abc475/c.js
+++ abc475/c.js
@@ -5,54 +5,17 @@ S = Number(S) - 1;
 const A = input[1].split(' ').map(BigInt);
 
 const B = [0n];
-for (let i = 0; i < A.length; i++) {
+for (let i = 0; i < N - 1; i++) {
   B[i + 1] = B[i] + A[i];
 }
 
-const abs = (a) => {
-  return a >= 0n ? a : -a;
-};
-
-// console.log(B.join(' '));
-
 let ans = 1;
-for (let i = 0; i < N; i++) {
-  const diff = B[S] - B[i];
-  if (diff === 0n) {
-    continue;
+for (let i = 0; i <= S; i++) {
+  for (let j = S; j < N; j++) {
+    const xl = B[S] - B[i];
+    const xr = B[j] - B[S];
+    if (2n * xl + xr <= L) ans = Math.max(ans, j - i + 1);
+    if (2n * xr + xl <= L) ans = Math.max(ans, j - i + 1);
   }
-
-  const distance = abs(diff);
-  if (distance > L) {
-    continue;
-  }
-
-  let rem = L - distance;
-  let cur = Math.abs(S - i) + 1;
-  ans = Math.max(ans, cur);
-
-  if (diff > 0) {
-    for (let j = S; j < A.length; j++) {
-      if (rem - A[j] * 2n >= 0) {
-        rem -= A[j] * 2n;
-        cur++;
-      } else {
-        break;
-      }
-    }
-    // console.log('A', { i, cur, distance });
-  } else {
-    for (let j = S - 1; j >= 0; j--) {
-      if (rem - A[j] * 2n >= 0) {
-        rem -= A[j] * 2n;
-        cur++;
-      } else {
-        break;
-      }
-    }
-    // console.log('B', { i, cur, distance });
-  }
-
-  ans = Math.max(ans, cur);
 }
 console.log(ans);
